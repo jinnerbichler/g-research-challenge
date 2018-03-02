29af500dd3f6865695d91818a439f59ae42b312d
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-03-02 01:02:49 +0100

    worked on first version of net

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import pandas as pd
+import numpy as np
 import tensorflow as tf
 
-train_data = pd.read_csv('data/train.csv', index_col=0)
-test_data = pd.read_csv('data/test.csv', index_col=0)
+tf.logging.set_verbosity(tf.logging.INFO)
+
 NUMERICAL_COLS = ['x0', 'x1', 'x2', 'x3A', 'x3B', 'x3C', 'x3D', 'x3E', 'x4', 'x5', 'x6']
-all_data = pd.concat([train_data, test_data])
+NUM_HIDDEN_UNITS = [2048, 1024, 512, 256]
 
 
 # normalize numerical data
-def normalize(train, test):
-    all = pd.concat([train, test])
-    normalized_data = (all - all.min()) / (all.max() - all.min())
+def normalize_data(train, test):
+    all_data = pd.concat([train, test])
+    normalized_data = (all_data - all_data.min()) / (all_data.max() - all_data.min())
     normalized_train = normalized_data.iloc[0:len(train)]
     normalized_test = normalized_data.iloc[len(train):]
 
@@ -18,67 +19,100 @@ def normalize(train, test):
     test[NUMERICAL_COLS] = normalized_test[NUMERICAL_COLS]
 
 
-# normalize continous data
-normalize(train=train_data, test=test_data)
+def g_model_fn(features, labels, mode, params):
+    is_training = mode == tf.estimator.ModeKeys.TRAIN
 
-# create one-hot encoding
-num_days = all_data['Day'].max()
-num_markets = all_data['Market'].max()
-num_stocks = all_data['Stock'].max()
+    with tf.name_scope('input'):
+        days_one_hot = tf.one_hot(features['days'], depth=params['num_days'])
+        markets_one_hot = tf.one_hot(features['markets'], depth=params['num_markets'])
+        stocks_one_hot = tf.one_hot(features['stocks'], depth=params['num_stocks'])
 
-with tf.name_scope('input'):
-    day_ph = tf.placeholder(tf.int32, shape=[None], name='day')
-    market_ph = tf.placeholder(tf.int32, shape=[None], name='market')
-    stock_ph = tf.placeholder(tf.int32, shape=[None], name='stock')
-    numerical_ph = tf.placeholder(tf.float32, shape=(None, len(NUMERICAL_COLS)), name='numeric_input')
-    train_y_ph = tf.placeholder(tf.float32, shape=[None], name='training_y')
-    train_w_ph = tf.placeholder(tf.float32, shape=[None], name='training_w')
+        net = tf.concat([days_one_hot, markets_one_hot, stocks_one_hot, features['numerical_input']], 1)
 
-    days_one_hot = tf.one_hot(day_ph, depth=num_days)
-    markets_one_hot = tf.one_hot(market_ph, depth=num_markets)
-    stocks_one_hot = tf.one_hot(stock_ph, depth=num_stocks)
+    with tf.name_scope('net'):
+        for hidden_unit in NUM_HIDDEN_UNITS:
+            net = tf.layers.dense(inputs=net, units=hidden_unit, activation=tf.nn.relu)
+            net = tf.layers.dropout(inputs=net, rate=params['dropout_rate'], training=is_training)
+        predictions = tf.layers.dense(inputs=net, units=1)
 
-    all_inputs = tf.concat([days_one_hot, markets_one_hot, stocks_one_hot, numerical_ph], 1)
+    # check if inference
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-with tf.name_scope('net'):
-    dense = tf.layers.dense(inputs=all_inputs, units=1024, activation=tf.nn.relu)
-    dropout = tf.layers.dropout(inputs=dense, rate=0.4, training=True)
-    predicted_y = tf.layers.dense(inputs=dropout, units=1)
+    with tf.name_scope('loss'):
+        # weighted MSE
+        loss = tf.reduce_mean(tf.pow(labels - predictions, 2) * features['weights'], name='loss')
 
-with tf.name_scope('loss'):
-    loss = tf.reduce_mean(tf.pow(train_y_ph - predicted_y, 2) * train_w_ph, name='loss')  # weighted MSE
+    metrics = {
+        "accuracy": tf.metrics.accuracy(labels=labels, predictions=predictions)
+    }
 
-with tf.name_scope('optimizer'):
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-    train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(loss=loss, global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op, eval_metric_ops=metrics)
 
-init = tf.global_variables_initializer()
+    # Add evaluation metrics (for EVAL mode)
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=metrics)
 
-# Start training
-with tf.Session() as sess:
-    # Run the initializer
-    sess.run(init)
 
-    for epoch in range(len(train_data)):
-        epoch_data = train_data.ix[epoch]
-        day = [epoch_data['Day']]
-        market = [epoch_data['Market']]
-        stock = [epoch_data['Stock']]
-        numerical_data = [epoch_data[NUMERICAL_COLS].values]
-        train_y = [epoch_data['y']]
-        train_w = [epoch_data['Weight']]
+def main(argv):
+    train_data = pd.read_csv('data/train.csv', index_col=0).fillna(method='ffill')
+    test_data = pd.read_csv('data/test.csv', index_col=0).fillna(method='ffill')
+    all_data = pd.concat([train_data, test_data])
 
-        _, _, loss_r = sess.run([train_op, days_one_hot, loss], feed_dict={day_ph: day,
-                                                                market_ph: market,
-                                                                stock_ph: stock,
-                                                                numerical_ph: numerical_data,
-                                                                train_y_ph: train_y,
-                                                                train_w_ph: train_w})
+    # normalize continous data
+    normalize_data(train=train_data, test=test_data)
 
-        print(loss_r)
+    # create one-hot encoding
+    params = {
+        'num_days': all_data['Day'].max(),
+        'num_markets': all_data['Market'].max(),
+        'num_stocks': all_data['Stock'].max(),
+        'dropout_rate': 0.4
+    }
 
-        # # Display logs per epoch step
-        # if (epoch+1) % display_step == 0:
-        #     c = sess.run(cost, feed_dict={X: train_X, Y:train_Y})
-        #     print("Epoch:", '%04d' % (epoch+1), "cost=", "{:.9f}".format(c), \
-        #         "W=", sess.run(W), "b=", sess.run(b))
+    input_train = {
+        'days': train_data['Day'].values,
+        'markets': train_data['Market'].values,
+        'stocks': train_data['Stock'].values,
+        'weights': train_data['Weight'].values.astype(np.float32),
+        'numerical_input': train_data[NUMERICAL_COLS].values.astype(np.float32)
+    }
+
+    train_input_fn = tf.estimator.inputs.numpy_input_fn(
+        x=input_train,
+        y=train_data['y'].values.astype(np.float32),
+        batch_size=100,
+        num_epochs=None,
+        shuffle=True)
+
+    # Create the Estimator
+    g_regression = tf.estimator.Estimator(model_fn=g_model_fn,
+                                          params=params,
+                                          model_dir="/tmp/g_model")
+
+    # train the estimator
+    g_regression.train(input_fn=train_input_fn, steps=20000)
+
+    # perform inference
+    input_predict = {
+        'days': test_data['Day'].values,
+        'markets': test_data['Market'].values,
+        'stocks': test_data['Stock'].values,
+        'numerical_input': test_data[NUMERICAL_COLS].values.astype(np.float32)
+    }
+    predict_input_fn = tf.estimator.inputs.numpy_input_fn(x=input_predict,
+                                                          num_epochs=1,
+                                                          shuffle=False)
+    predictions = g_regression.predict(input_fn=predict_input_fn)
+    predictions = [p[0] for p in predictions]
+
+    # prepare submission
+    submission = pd.DataFrame(predictions, columns=['y'])
+    submission.index.name = 'Index'
+    submission.to_csv('submission/submission.csv')
+
+
+if __name__ == "__main__":
+    tf.app.run(main)
