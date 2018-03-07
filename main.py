72a83b5219c26b60f19e6250e9c270e797df46d2
Johannes Innerbichler <j.innerbichler@gmail.com>
2018-03-07 18:08:48 +0100

    new functionality of server

--- main.py
+++ main.py
@@ -19,12 +19,16 @@ def main(argv):
         [1024]
     ]
 
-    for lr in reversed([0.01, 0.001, 0.0001, 0.00001]):
-        for dr in [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
-            for hu in hidden_units:
-                for shuffle in [False, True]:
-                    execute(learning_rate=lr, dropout_rate=dr, hidden_units=hu, shuffle=shuffle,
-                            batch_size=1, l2_reg_scale=0.1)
+    # for lr in reversed([0.01, 0.001]):
+    #     for dr in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]:
+    #         for hu in hidden_units:
+    #             for reg_scale in reversed([0.1, 0.01, 0.001, 0.0001]):
+    #                 for shuffle in [True]:
+    #                     execute(learning_rate=lr, dropout_rate=dr, hidden_units=hu, shuffle=shuffle,
+    #                             batch_size=1, l2_reg_scale=reg_scale)
+
+    execute(learning_rate=0.01, dropout_rate=0.0, hidden_units=[1024 * 2, 1024], shuffle=True,
+            batch_size=10, l2_reg_scale=0.0)
 
 
 # normalize numerical data
@@ -42,11 +46,12 @@ def g_model_fn(features, labels, mode, params):
     is_training = mode == tf.estimator.ModeKeys.TRAIN
 
     with tf.name_scope('input'):
-        days_one_hot = tf.one_hot(features['days'], depth=params['num_days'])
-        markets_one_hot = tf.one_hot(features['markets'], depth=params['num_markets'])
-        stocks_one_hot = tf.one_hot(features['stocks'], depth=params['num_stocks'])
+        days_one_hot = tf.one_hot(features['days'], depth=params['num_days'], name='day_one_hot')
+        markets_one_hot = tf.one_hot(features['markets'], depth=params['num_markets'], name='market_one_hot')
+        stocks_one_hot = tf.one_hot(features['stocks'], depth=params['num_stocks'], name='stocks_one_hot')
 
-        net = tf.concat([days_one_hot, markets_one_hot, stocks_one_hot, features['numerical_input']], 1)
+        merged = [days_one_hot, markets_one_hot, stocks_one_hot, features['numerical_input']]
+        net = tf.concat(values=merged, axis=1, name='merged_input')
 
     with tf.name_scope('net'):
         for hidden_unit in params['hidden_units']:
@@ -68,18 +73,18 @@ def g_model_fn(features, labels, mode, params):
 
     with tf.name_scope('loss'):
         # weighted MSE
-        wmse_loss = tf.reduce_mean(tf.pow(labels['y'] - predictions, 2) * features['weights'], name='loss')
-
-        # weight loss
+        wmse_loss = tf.reduce_mean(tf.pow(labels['y'] - predictions, 2) * labels['weight'],
+                                   name='wmse_loss')
 
         # l2 regularizer
         l2_loss = tf.losses.get_regularization_loss()
 
         # total loss
-        loss = wmse_loss + l2_loss
+        loss = tf.add(wmse_loss, l2_loss, name='total_loss')
 
-    tf.summary.scalar('wmse_loss', wmse_loss)
-    tf.summary.scalar('l2_loss', l2_loss)
+    tf.summary.scalar('wmse_loss', wmse_loss, family='losses')
+    tf.summary.scalar('l2_loss', l2_loss, family='losses')
+    tf.summary.scalar('total_loss', loss, family='losses')
 
     if mode == tf.estimator.ModeKeys.TRAIN:
         optimizer = tf.train.GradientDescentOptimizer(learning_rate=params['learning_rate'])
@@ -87,10 +92,7 @@ def g_model_fn(features, labels, mode, params):
         return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
     # Add evaluation metrics (for EVAL mode)
-    metrics = {
-        'accuracy': tf.metrics.mean_squared_error(predictions=predictions, labels=labels['y'])
-    }
-    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=metrics)
+    return tf.estimator.EstimatorSpec(mode=mode, loss=wmse_loss)
 
 
 def execute(learning_rate, dropout_rate, hidden_units, shuffle, batch_size, l2_reg_scale):
@@ -101,8 +103,8 @@ def execute(learning_rate, dropout_rate, hidden_units, shuffle, batch_size, l2_r
     # normalize continous data
     normalize_data(train=train_data, test=test_data)
 
-    # train_data = train_data.ix[:10]
-    train_data, eval_data = train_test_split(train_data, test_size=0.25, shuffle=shuffle)
+    # train_data = train_data.ix[:100]
+    train_data, eval_data = train_test_split(train_data, test_size=0.95, shuffle=shuffle)
 
     # create hyperparameter
     params = {
@@ -126,11 +128,13 @@ def execute(learning_rate, dropout_rate, hidden_units, shuffle, batch_size, l2_r
                                                         batch_size=batch_size,
                                                         num_epochs=1,
                                                         shuffle=shuffle)
-    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=len(train_data) // batch_size * 20)
+    max_steps = len(train_data) // batch_size * 2000000000
+    train_spec = tf.estimator.TrainSpec(input_fn=train_input_fn, max_steps=max_steps)
 
     # specify validation
     eval_input_fn = tf.estimator.inputs.numpy_input_fn(x=make_features(eval_data),
                                                        y=make_target(eval_data),
+                                                       batch_size=1,
                                                        num_epochs=1,
                                                        shuffle=False)
     eval_spec = tf.estimator.EvalSpec(input_fn=eval_input_fn, steps=None,
@@ -144,36 +148,36 @@ def execute(learning_rate, dropout_rate, hidden_units, shuffle, batch_size, l2_r
     # train and validate the estimator
     tf.estimator.train_and_evaluate(estimator=g_regression_model, train_spec=train_spec, eval_spec=eval_spec)
 
-    # perform inference
-    predict_input_fn = tf.estimator.inputs.numpy_input_fn(x=make_features(test_data),
-                                                          num_epochs=1,
-                                                          shuffle=False)
-    predictions = g_regression_model.predict(input_fn=predict_input_fn)
-    predictions = [p[0] for p in predictions]  # convert arrays to float
+    # # prepare submission
+    # predict_input_fn = tf.estimator.inputs.numpy_input_fn(x=make_features(test_data),
+    #                                                       num_epochs=1,
+    #                                                       shuffle=False)
+    # predictions = g_regression_model.predict(input_fn=predict_input_fn)
+    # predictions = [p['y'][0] for p in predictions]  # convert arrays to float
+    #
+    # submission = pd.DataFrame(predictions, columns=['y'])
+    # submission.index.name = 'Index'
+    # os.makedirs('./submissions/', exist_ok=True)
+    # submission.to_csv('./submissions/submission_{}.csv'.format(model_name))
 
-    # prepare submission
-    submission = pd.DataFrame(predictions, columns=['y'])
-    submission.index.name = 'Index'
-    os.makedirs('./submissions/', exist_ok=True)
-    submission.to_csv('./submissions/submission_{}.csv'.format(model_name))
+    # compute last validation
+    eval_predictions = g_regression_model.evaluate(input_fn=eval_input_fn, steps=None)
+    return eval_predictions['loss']
 
 
 def parameter_to_name(params):
-    return 'LR{}_DR{}_HU{}_SH{}_RS{}_BS{}'.format(
-        params['learning_rate'], params['dropout_rate'],'-'.join([str(x) for x in params['hidden_units']]),
+    return 'LR{:.7f}_DR{:.1f}_HU{}_SH{}_RS{:.5f}_BS{}'.format(
+        params['learning_rate'], params['dropout_rate'], '-'.join([str(x) for x in params['hidden_units']]),
         params['shuffle'], params['reg_scale'], params['batch_size'])
 
 
 def make_features(dataframe):
-    input_dict = {
+    return {
         'days': dataframe['Day'].values,
         'markets': dataframe['Market'].values,
         'stocks': dataframe['Stock'].values,
         'numerical_input': dataframe[NUMERICAL_COLS].values.astype(np.float32)
     }
-    if 'Weight' in dataframe.columns:
-        input_dict['weights'] = dataframe['Weight'].values.astype(np.float32)
-    return input_dict
 
 
 def make_target(dataframe):
